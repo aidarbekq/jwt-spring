6a0e7b9427e00f7ac72828005a1a04a463cb0847
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-04-07 22:14:22 +0600

    add project

---
+++ src/test/java/com/pract/jwtspring/JwtSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pract.jwtspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
