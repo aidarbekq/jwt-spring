30dfe9de8baef09ca5451375fe30d457950971e3
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-04-09 14:51:56 +0600

    add dto

---
+++ src/main/java/com/pract/jwtspring/dto/TokenDto.java
@@ -0,0 +1,16 @@
+package com.pract.jwtspring.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TokenDto {
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+
+}
